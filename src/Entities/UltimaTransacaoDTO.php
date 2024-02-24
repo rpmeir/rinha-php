77b2dc13064444122d5ce85253cf7438a503a97a
Rodrigo Pires Meira <rpmeir@gmail.com>
2024-02-23 23:59:18 -0300

    Ignore phpunit result cache and adjust last transaction

--- src/Entities/UltimaTransacaoDTO.php
+++ src/Entities/UltimaTransacaoDTO.php
@@ -5,8 +5,6 @@ namespace Rinha\Entities;
 class UltimaTransacaoDTO
 {
     private function __construct(
-        public readonly int $id,
-        public readonly int $conta_id,
         public readonly int $valor,
         public readonly string $tipo,
         public readonly string $descricao,
